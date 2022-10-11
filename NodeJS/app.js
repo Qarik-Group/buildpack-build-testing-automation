b3a61198f5081b303ac6ff0f5865b91179d7c145
Chad Kullhem <digitaleagle@gmail.com>
2022-10-11 11:45:05 -0600

    Added NodeJS test

---
+++ NodeJS/app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const PORT = process.env.PORT || 8080;
+const server = http.createServer(async (req, res) => {
+    //set the request route
+    if (req.url === "/api" && req.method === "GET") {
+        //response headers
+        res.writeHead(200, { "Content-Type": "application/json" });
+        //set the response
+        res.write("{ test: \"success\"}");
+        //end the response
+        res.end();
+    }
+
+    // If no route present
+    else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+});
